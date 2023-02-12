8ba9ce876f8fadb59b28fdcba60aaf515b8070f5
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2023-02-12 11:28:47 -0300

    Add draft

--- src/Matrix.java
+++ src/Matrix.java
@@ -5,11 +5,23 @@ Assuming we start at matrix[0][0], and can only move right or down, find the max
 For example, in this matrix
 0 3 1 1
 2 0 0 4
-1 5 2 1
+1 5 3 1
 The most we can collect is: 0 + 2 + 1 + 5 + 3 + 1 = 12 coins
 */
 public class Matrix {
     public static void main(String[] args) {
 
+        int[][] matrix = {{0, 3, 1, 1}, {2, 0, 0, 4}, {1, 5, 3, 1}};
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Element [" + i + ", " + j + "]: ");
+                sum = sum + matrix[i][j];
+            }
+        }
+        System.out.println("\n\nSum of the elements: " + sum);
     }
+
+
 }
